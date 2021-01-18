dc08972e4817d7f5d529b7ba821ef7ca3654ac92
sergio_prog@outlook.com <sergio_prog@outlook.com>
2021-01-18 15:22:12 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BoilerControl
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public class Test
+    {
+         public int TT { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        Test test = new Test();
+        Test test1 = new Test();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            test.TT = 1;
+           
+            test1.TT = 2;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BoilerRoom boilerRoom = new BoilerRoom();
+            boilerRoom.Show();
+        }
+
+
+    }
+}
